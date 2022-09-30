13565a21095ec5e906fd12652ba47cc8ee4596d2
TAMARA <75030936+T-A-T-I-A@users.noreply.github.com>
2022-09-30 20:53:41 +0200

    Create 2.27 Figura a scacchiera di asterischi.c

---
+++ 2.27 Figura a scacchiera di asterischi.c
@@ -0,0 +1,14 @@
+//stampa figura di scacchiera con gli asterischi
+#include <stdio.h>
+
+int main()
+{
+	printf("* * * * * * * * \n"
+	   " * * * * * * * *\n"
+	    "* * * * * * * * \n"
+	   " * * * * * * * *\n"
+	    "* * * * * * * * \n"
+	   " * * * * * * * *\n"
+	    "* * * * * * * * \n"
+	   " * * * * * * * *\n");
+}
